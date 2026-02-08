5cb338ee56f205e733b2c5e3c37d0f9ca919d583
Tass-1 <priyanshujoshi10000@gmail.com>
2026-02-08 14:47:29 +0530

    small change

--- src/components/airdropModal.tsx
+++ src/components/airdropModal.tsx
@@ -16,14 +16,18 @@ export function Modal({open , close} : {
     <>
     {open && (<div className='w-screen h-screen flex items-center fixed inset-0 justify-center bg-[#13171e]/60 backdrop-blur-md z-50'>
         
-<div className='bg-[#13171e] h-80 w-80'>
-      <button className='bg-red-200' onClick={close}>Click me </button>
+<div className='bg-[#1c202a] h-100 w-150 rounded-xl'>
+      
       <ConnectionProvider endpoint='https://api.devnet.solana.com' >
         <WalletProvider  wallets={[]}>
           <WalletModalProvider>
+              <button className='bg-red-200' onClick={close}>Click me </button>
 
-              <WalletDisconnectButton></WalletDisconnectButton>
-              <WalletMultiButton></WalletMultiButton>
+              <div>
+                  <WalletDisconnectButton></WalletDisconnectButton>
+                  <WalletMultiButton></WalletMultiButton>
+              </div>
+              
               <Airdrop></Airdrop>
           </WalletModalProvider>
         </WalletProvider>
